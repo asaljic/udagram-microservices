e6fb8e2f85f8ad9fa5d683591866057d9325d7fa
asaljic <amar.logins@outlook.com>
2021-01-26 09:27:23 +0100

    chore: Added build of reverse proxy to Travis CI pipeline

--- build.sh
+++ build.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 PROJECT=$1
-cd $PROJECT
-npm ci
-cd ..
 
-if [ $PROJECT = 'udagram-frontend' ]; then
-	./build_frontend.sh $PROJECT
-else 
-    ./build_node.sh $PROJECT
+if [$PROJECT = 'udagram-frontend'] || [$PROJECT = 'udagram-feed'] || [$PROJECT = 'udagram-user'];
+then
+    cd $PROJECT
+    npm ci
+    cd ..
+    if [ $PROJECT = 'udagram-frontend' ]; 
+    then
+        ./build_frontend.sh $PROJECT
+    else
+        ./build_node.sh $PROJECT
+    fi
 fi
+
 
 cd $PROJECT
 docker build -t $PROJECT .
